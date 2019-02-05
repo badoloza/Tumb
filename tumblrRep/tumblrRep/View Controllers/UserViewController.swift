e8ecf26c426d0c3f0cb65be7a72db8d2eed8dc0b
MacbookPro <macbookpro@sfs-wifi-dhcp-10-143-176-238.wifi.sfsu.edu>
2019-02-04 19:54:54 -0800

    created Tumb

---
+++ tumblrRep/tumblrRep/View Controllers/UserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserViewController.swift
+//  tumblrRep
+//
+//  Created by MacbookPro on 2/4/19.
+//  Copyright © 2019 Eduardo Loza. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tumblrRep/tumblrRep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tumblrRep
+//
+//  Created by MacbookPro on 2/4/19.
+//  Copyright © 2019 Eduardo Loza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
